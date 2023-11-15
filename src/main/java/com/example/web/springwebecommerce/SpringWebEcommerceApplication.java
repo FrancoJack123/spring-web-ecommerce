83def77c00898dcb852960279a054b09a494f51c
FrancoJack123 <jackfrancomoreno15@gmail.com>
2023-11-15 02:56:26 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/example/web/springwebecommerce/SpringWebEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.web.springwebecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web/springwebecommerce/SpringWebEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web.springwebecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
